2347179841d44b5f20ccf2211bc6f47df5d32222
tyoui <zw13312324165>
2019-02-19 19:52:27 +0800

    删除文件

--- jtyoui/data/constant.py
+++ jtyoui/data/constant.py
@@ -8,4 +8,4 @@ Photo_Format = (
     'bmp', 'jpg', 'png', 'tif', 'gif', 'pcx', 'tga', 'exif', 'fpx', 'svg', 'psd', 'cdr', 'pcd', 'dxf', 'ufo', 'eps',
     'ai', 'raw', 'WMF', 'webp')
 
-DECODE = ('Unicode', 'ASCII', 'GBK', 'GB2312', 'UTF-8', 'ISO-8859-1', 'UTF-16', 'GB18030')
+DECODE = ('Unicode', 'ASCII', 'GBK', 'GB2312', 'UTF-8', 'ISO-8859-1', 'UTF-16', 'GB18030', 'ISO-8859-2')
