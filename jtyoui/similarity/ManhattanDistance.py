fc81d9cb6729bf349632d95441d9326ac271d533
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-03-20 18:32:52 +0800

    增加相似距离

--- jtyoui/similarity/ManhattanDistance.py
+++ jtyoui/similarity/ManhattanDistance.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time  : 2019/3/18 9:33
 # @Author: Jtyoui@qq.com
+from jtyoui.error import CoordinateLengthNotEqualError
 
 """
 曼哈顿距离
@@ -9,3 +10,20 @@
 ，驾驶距离显然不是两点间的直线距离。这个实际驾驶距离就是“曼哈顿距离”。
 曼哈顿距离也称为“城市街区距离”(City Block distance)。
 """
+
+
+def manhattan_distance(coordinate_p, coordinate_q):
+    """
+    曼哈顿距离
+    :param coordinate_p: p坐标
+    :param coordinate_q: q坐标
+    :return: 曼哈顿距离值
+    """
+    if len(coordinate_q) != len(coordinate_p):
+        raise CoordinateLengthNotEqualError("坐标长度不一致")
+    point = (abs(x - y) for x, y in zip(coordinate_q, coordinate_p))
+    return sum(point)
+
+
+if __name__ == '__main__':
+    print(manhattan_distance((1, 1), (7, 7)))
--- jtyoui/similarity/MinkowskiDistance.py
+++ jtyoui/similarity/MinkowskiDistance.py
@@ -2,7 +2,25 @@
 # -*- coding: utf-8 -*-
 # @Time  : 2019/3/18 17:16
 # @Author: Jtyoui@qq.com
+import os
 
 """
-闵可夫斯基距离(Minkowski Distance)，闵氏距离不是一种距离，而是一组距离的定义。
+闵可夫斯基距离
+闵氏空间指狭义相对论中由一个时间维和三个空间维组成的时空，
+为俄裔德国数学家闵可夫斯基(1864-1909)最先表述。
+他的平坦空间的概念以及表示为特殊距离量的几何学是与狭义相对论的要求相一致的。
+闵可夫斯基空间不同于牛顿力学的平坦空间
+当p=1时，得到绝对值距离，也叫曼哈顿距离
+当p=2时，得到欧几里德距离
+令p无穷大，得到切比雪夫距离
 """
+
+
+def minkowski_distance(coordinate_p, coordinate_q, dimension):
+    """
+    闵可夫斯基距离
+    :param coordinate_p: p坐标
+    :param coordinate_q: q坐标
+    :param dimension:闵可夫斯基维度
+    :return: 闵可夫斯基距离
+    """
