b5b8c7b67b73be19484e529c367de5da66449c99
弱水三千 <jtyoui@gmail.com>
2019-04-04 00:17:16 +0800

    更改free_header

--- jtyoui/web/__init__.py
+++ jtyoui/web/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
-# @Time : 2019/2/19 0019
+# @Time : 2019/2/19
 # @Email : jtyoui@qq.com
-from .header import *
-from .ua import random, ua
+from .header import header, free_header  # 封装header
+from .ua import random, ua  # 封装UA
--- jtyoui/web/header.py
+++ jtyoui/web/header.py
@@ -17,16 +17,12 @@ def header(request_headers):
     return headers
 
 
-free_headers = """Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-Accept-Encoding: gzip, deflate, br
-Accept-Language: zh-CN,zh;q=0.9
-Cache-Control: max-age=0
-Connection: keep-alive
-Cookie: UM_distinctid=168ff800e18655-08f0ece15a2295-4c277913-100200-168ff800e1b4b9; ASPSESSIONIDAZBYCXDW=1622273B73A768F308474040; Hm_lvt_353f6f980a356b6f65e5a65aad50c98e=1550208747,1550474802,1550474812,1550558911; Hm_lpvt_353f6f980a356b6f65e5a65aad50c98e=1550558911
-Host: www.xunleige.com
-If-Modified-Since: Tue, 19 Feb 2019 05:13:00 GMT
-If-None-Match: "e02c95c811c8d41:0"
-Upgrade-Insecure-Requests: 1
-User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6815.400 QQBrowser/10.3.3006.400"""
+Response_Headers = """accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3
+accept-language: zh-CN,zh;q=0.9,en;q=0.8
+cache-control: max-age=0
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"""
+
+free_header = header(Response_Headers)
+
 if __name__ == '__main__':
-    print(header(free_headers))
+    print(header(Response_Headers))
