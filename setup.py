912fcd9a2d55d047a7c73a8b26c06db1be7275fb
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-05-16 11:05:58 +0800

    更新版本19.5.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='jtyoui',
-    version='19.5.12',
+    version='19.5.16',
     description='This is my collection bag.',
     long_description=long_text,
     long_description_content_type="text/markdown",
