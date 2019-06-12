c66b4d24a9c81aa292b81ff734ce1b84ebb46be8
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-06-12 10:51:40 +0800

    更新版本

--- jtyoui/tools/_pip.py
+++ jtyoui/tools/_pip.py
@@ -6,13 +6,17 @@ import os
 import time
 
 
-def pips(module):
-    """安装模块"""
+def pips(module, package=None):
+    """安装模块：有时候加载模块和下载模块的名字不一样，需要指定package
+    :param module: 加载模块的名字
+    :param package: 没有模块下载模块的名字
+    """
     module = module.lower()
+    package = package.lower() if package else module
     try:
         m = __import__(module)
     except ModuleNotFoundError:
-        os.system('pip install --user ' + module)
+        os.system(F'pip install --user {package}')
         time.sleep(2)
         m = __import__(module)
     return m
