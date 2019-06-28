3a9ed9adb2102b802fdb4cfbefd814279345e1c2
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-06-28 10:12:59 +0800

    更新版本

--- jtyoui/__init__.py
+++ jtyoui/__init__.py
@@ -31,5 +31,5 @@ from jtyoui.interface import *  # 界面
 
 __all__ = [a for a in dir() if not a.startswith('_')]
 __all__.extend(['game', 'imagepdf', 'wx'])  # 游戏 pdf和照片互转 微信抓电影和聊天机器人
-__version__ = '19.6.19'
+__version__ = '19.6.28'
 __author__ = 'Jtyoui'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='jtyoui',
-    version='19.6.19',
+    version='19.6.28',
     description='This is my collection bag.',
     long_description=long_text,
     long_description_content_type="text/markdown",
