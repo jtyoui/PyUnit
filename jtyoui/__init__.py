6ad3833efd38ef6ea9f4468629a3bd531bfe9f56
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-07-17 10:01:50 +0800

    增加判断字符串是否属于某个列表的子集。

--- jtyoui/__init__.py
+++ jtyoui/__init__.py
@@ -31,5 +31,5 @@ from jtyoui.interface import *  # 界面
 
 __all__ = [a for a in dir() if not a.startswith('_')]
 __all__.extend(['game', 'imagepdf', 'wx'])  # 游戏 pdf和照片互转 微信抓电影和聊天机器人
-__version__ = '19.7.10'
+__version__ = '19.7.17'
 __author__ = 'Jtyoui'
--- jtyoui/data/methods.py
+++ jtyoui/data/methods.py
@@ -12,30 +12,35 @@ _special = "#$%&@"
 
 # 随机选择字母:
 def random_char(number=1):
+    """随机选择字母:number是生成个数"""
     ls = random.choices(string.ascii_letters, k=number)
     return ''.join(ls)
 
 
 # 随机选择小写字母:
 def random_lower_char(number=1):
+    """随机选择小写字母:number是生成个数"""
     ls = random.choices(string.ascii_lowercase, k=number)
     return ''.join(ls)
 
 
 # 随机选择大写字母:
 def random_upper_char(number=1):
+    """随机选择大写字母:number是生成个数"""
     ls = random.choices(string.ascii_uppercase, k=number)
     return ''.join(ls)
 
 
 # 随机选择数字:
 def random_digits(number=1):
+    """随机选择数字:number是生成个数"""
     ls = random.choices(string.digits, k=number)
     return ''.join(ls)
 
 
 # 随机选择特殊字符:
 def random_special(number=1):
+    """随机选择特殊字符:number是生成个数"""
     ls = random.choices(_special, k=number)
     return ''.join(ls)
 
@@ -63,6 +68,17 @@ def max_str(ls: list):
     return max(c.keys(), key=c.get)
 
 
+def contain_list_subset(str_: str, ls: list) -> (bool, list):
+    """
+    输入一个字符串判断字符串是否属于某个列表的子集。例如：str_：贵州，ls：[贵州省，遵义市]，那么贵州属于ls某个字符串的子集
+     :param str_: 字符串
+    :param ls: 字符串列表
+    :return: 存在返回True。不存在返回False。都会返回list列表
+    """
+    v = [subset for subset in ls if str_ in subset]
+    return any(v), v
+
+
 if __name__ == '__main__':
     print(random_char(4))
     print(random_lower_char(4))
@@ -72,3 +88,4 @@ if __name__ == '__main__':
     print(flag_contain_subset('我家住在北京', '家住、诉求、请求'.split('、')))
     print(contain_subset('我家住在北京', '家住、诉求、请求'.split('、')))
     print(max_str(['a', 'a', 'a', 'b', 'c', 'd', 'd']))
+    print(contain_list_subset('贵州', ['贵州省', '遵义市', '贵州省贵阳市']))
