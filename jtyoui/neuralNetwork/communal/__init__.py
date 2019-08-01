36095d47ab600cf415d1762ff7fd738053eec9c3
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-08-01 17:46:25 +0800

    增加快速读取序列数据

---
+++ jtyoui/neuralNetwork/communal/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2019/8/1 17:43
+# @Author: Jtyoui@qq.com
---
+++ jtyoui/neuralNetwork/communal/load_label_data.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2019/8/1 15:53
+# @Author: Jtyoui@qq.com
+import copy
+
+
+def get_vocab(path, tags):
+    data, label, vocab = [], [], {}
+    # <PAD>：0, <UNK>：1, <END>：2
+    i = 3
+    with open(path, encoding='utf-8') as fp:
+        ds, ls = [], []
+        for line in fp:
+            line = line.strip()
+            if line:
+                k, v = line.split()
+                if k not in vocab:
+                    vocab[k] = i
+                    i += 1
+                ds.append(vocab[k])
+                ls.append(tags[v])
+            else:
+                data.append(copy.deepcopy(ds + [2]))
+                label.append(copy.deepcopy(ls + [2]))
+                ds, ls = [], []
+
+    return data, label, vocab
+
+
+if __name__ == '__main__':
+    tag = {'O': 0,
+           'B-a': 1, 'I-a': 2,
+           'B-b': 3, 'I-b': 4,
+           'B-c': 5, 'I-c': 6
+           }
+    train, test, vocabs = get_vocab(r'train.txt', tag)
