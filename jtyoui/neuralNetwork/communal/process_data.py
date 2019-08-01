1d92f1af5c6dbaecc20f59987573271ad4278515
jtyoui <zw13312324165>
2019-08-01 21:40:48 +0800

    修改提取数据

--- jtyoui/neuralNetwork/communal/process_data.py
+++ jtyoui/neuralNetwork/communal/process_data.py
@@ -3,14 +3,16 @@ from collections import Counter
 from keras.preprocessing.sequence import pad_sequences
 
 
-def load_data(train_path, dev_path):
-    train = _parse_data(open('data/train.txt', 'rb'))
+def load_data(train_path, dev_path=None):
+    train = _parse_data(open(train_path, 'rb'))
     word_counts = Counter(row[0].lower() for sample in train for row in sample)
     vocab = [w for w, f in iter(word_counts.items()) if f >= 2]
     chunk_tags = ['O', 'B-a', 'I-a', 'B-b', 'I-b', "B-c", "I-c"]
     train = _process_data(train, vocab, chunk_tags)
-    # test = _parse_data(open('data/test_data.data', 'rb'))
-    # test = _process_data(vocab, chunk_tags)
+    if dev_path:
+        dev = _parse_data(open(dev_path, 'rb'))
+        dev = _process_data(dev, vocab, chunk_tags)
+        return train, dev, (vocab, chunk_tags)
     return train, (vocab, chunk_tags)
 
 
@@ -22,24 +24,24 @@ def _parse_data(fh, split_text='\n'):
     return data
 
 
-def _process_data(data, vocab, chunk_tags, maxlen=None, onehot=False):
-    if maxlen is None:
-        maxlen = max(len(s) for s in data)
+def _process_data(data, vocab, chunk_tags, max_len=None, one_hot=False):
+    if max_len is None:
+        max_len = max(len(s) for s in data)
     word2idx = dict((w, i) for i, w in enumerate(vocab))
-    x = [[word2idx.get(w[0].lower(), 1) for w in s] for s in data]  # set to <unk> (index 1) if not in vocab
+    x = [[word2idx.get(w[0].lower(), 1) for w in s] for s in data]
     y_chunk = [[chunk_tags.index(w[1]) for w in s] for s in data]
-    x = pad_sequences(x, maxlen)  # left padding
-    y_chunk = pad_sequences(y_chunk, maxlen, value=-1)
-    if onehot:
+    x = pad_sequences(x, max_len)
+    y_chunk = pad_sequences(y_chunk, max_len, value=-1)
+    if one_hot:
         y_chunk = numpy.eye(len(chunk_tags), dtype='float32')[y_chunk]
     else:
         y_chunk = numpy.expand_dims(y_chunk, 2)
     return x, y_chunk
 
 
-def process_data(data, vocab, maxlen=100):
+def process_data(data, vocab, max_len=100):
     word2idx = dict((w, i) for i, w in enumerate(vocab))
     x = [word2idx.get(w[0].lower(), 1) for w in data]
     length = len(x)
-    x = pad_sequences([x], maxlen)  # left padding
+    x = pad_sequences([x], max_len)
     return x, length
