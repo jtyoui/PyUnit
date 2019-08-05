9ee49c6481ab063696ceeca5f97954209cc5cd54
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-08-05 15:33:00 +0800

    优化断句算法

--- jtyoui/word/__init__.py
+++ jtyoui/word/__init__.py
@@ -8,3 +8,4 @@ from .ThreadNeologism import thread_analysis  # 有多线程
 from .NAA import NAA  # 自动过滤的算法
 from .TS import TextSummary  # 精简摘要算法
 from .tfidf import TFIDF  # 增加TFIDF
+from ._pt import *  # 根据词语进行重新断句
