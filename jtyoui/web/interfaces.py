13fc5c7036ec9e6b71ad1d18f2dfd206dc451a77
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-10-21 15:43:52 +0800

    简单接口测试

--- jtyoui/web/interfaces.py
+++ jtyoui/web/interfaces.py
@@ -25,7 +25,25 @@ def interface_test(dict_, address, record_time=True, **kwargs):
     return page
 
 
+def interface_tests(method, url, data, record_time=True, **kwargs):
+    """简单接口测试
+    :param method: 请求方法，比如：post、get
+    :param url: url地址
+    :param data: 传入的参数
+    :param record_time: 是否记录消耗时间
+    :param kwargs: 参考requests.post **kwargs
+    :return: 接口返回值
+    """
+    start = time.time()
+    response = requests.request(method=method.upper(), url=url, data=data, **kwargs)
+    end = time.time()
+    if record_time:
+        return response.text, end - start
+    return response.text
+
+
 interface_post_test = interface_test  # post接口测试
+interface_all_tests = interface_tests  # 接口测试
 
 if __name__ == '__main__':
     d = {'answer': '我要告南明区政府贪污。', 'event_id': 'df99f4bb7f94c69c1b37ece4b41f1d05'}
