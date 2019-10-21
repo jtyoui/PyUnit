488a2935ac2f234a0b228c1cf79a0066b5ffb3f1
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-10-21 13:54:21 +0800

    增加测试接口参数（header）

--- jtyoui/web/interfaces.py
+++ jtyoui/web/interfaces.py
@@ -7,23 +7,25 @@ import time
 import requests
 
 
-def interface_test(dict_, address, header=None, record_time=True):
+def interface_test(dict_, address, record_time=True, **kwargs):
     """简单接口测试
     :param dict_: 传入的参数
     :param address: url地址
-    :param header:header参数
+    :param kwargs:参考requests.post **kwargs
     :param record_time: 是否记录消耗时间
     :return: 接口返回值
     """
     start = time.time()
     j = json.dumps(dict_, ensure_ascii='utf8')
-    response = requests.post(address, j, headers=header)
+    response = requests.post(address, j, **kwargs)
     page = json.loads(response.text)
     end = time.time()
     if record_time:
         return page, end - start
     return page
 
+
+interface_post_test = interface_test  # post接口测试
 
 if __name__ == '__main__':
     d = {'answer': '我要告南明区政府贪污。', 'event_id': 'df99f4bb7f94c69c1b37ece4b41f1d05'}
