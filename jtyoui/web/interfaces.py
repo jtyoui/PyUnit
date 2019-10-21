f26280eb259d58ec012d8c3e36320d114d82433f
tyoui.zhang@xiaoi.com <Zw13312324165>
2019-10-21 13:43:27 +0800

    增加测试接口参数（header）

--- jtyoui/web/interfaces.py
+++ jtyoui/web/interfaces.py
@@ -7,16 +7,17 @@ import time
 import requests
 
 
-def interface_test(dict_, address, record_time=True):
+def interface_test(dict_, address, header=None, record_time=True):
     """简单接口测试
     :param dict_: 传入的参数
     :param address: url地址
+    :param header:header参数
     :param record_time: 是否记录消耗时间
     :return: 接口返回值
     """
     start = time.time()
     j = json.dumps(dict_, ensure_ascii='utf8')
-    response = requests.post(address, j)
+    response = requests.post(address, j, headers=header)
     page = json.loads(response.text)
     end = time.time()
     if record_time:
