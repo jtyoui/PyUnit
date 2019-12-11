731f015e82c08453021e190dfb76b25c8e5ddffc
jtyoui <jtyoui@qq.com>
2019-12-11 15:58:54 +0800

    增加docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,14 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
+# import os
+# import sys
 import sphinx_rtd_theme
 import time
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
+master_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = 'jtyoui'
@@ -61,5 +61,4 @@ exclude_patterns = []
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
-sys.path.insert(0, os.path.abspath('../../'))
-master_doc = 'index'
+# sys.path.insert(0, os.path.abspath('..'))
